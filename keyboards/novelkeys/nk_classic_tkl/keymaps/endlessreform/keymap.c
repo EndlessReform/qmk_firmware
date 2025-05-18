235f0d74fdd759f8e4f71d823d8c1d9dd39e5861
Jacob Keisling <jacob@keisling.me>
2025-05-18 16:51:14 -0500

    Fix macOS mode

--- keyboards/novelkeys/nk_classic_tkl/keymaps/endlessreform/keymap.c
+++ keyboards/novelkeys/nk_classic_tkl/keymaps/endlessreform/keymap.c
@@ -15,16 +15,19 @@
  */
 #include QMK_KEYBOARD_H
 
+#define _BASE 0
+#define _MAC 1
+#define _FN 2
 #define _MD 4
 #define _GIT 5
 
-enum my_keycodes {
-  RETRO_RGB = QK_USER_0
-};
-
 enum macro_keycodes {
+    /* Base layers*/
+    RETRO_RGB = QK_USER_0,
+    MAC_TOGGLE,
+
     /* Markdown */
-    HED1 = SAFE_RANGE, HED2, HED3,
+    HED1, HED2, HED3,
     TAGGED, PYTHON,
     CDFC, ALSO, DIVIDER,
     TASK, BASH,
@@ -40,29 +43,29 @@ enum macro_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_tkl_ansi_tsangan( /* Base */
+[_BASE] = LAYOUT_tkl_ansi_tsangan( /* Base */
     KC_ESC,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           C(S(G(KC_4))), C(G(KC_Q)), KC_PAUS,
     KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP,
     KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_END,  KC_PGDN,
     LCTL_T(KC_ESC), KC_A,    KC_S,   KC_D,   LT(_GIT, KC_F), KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    LT(_MD, KC_SCLN), KC_QUOT,          KC_ENT,
     KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                   KC_UP,
-    LT(1, KC_CAPS), KC_LGUI, KC_LALT,                KC_SPC,                                            KC_RALT, C(S(KC_LGUI)),   S(KC_LCTL), KC_LEFT, KC_DOWN, KC_RGHT),
+    LT(_FN, KC_CAPS), KC_LGUI, KC_LALT,                KC_SPC,                                            KC_RALT, S(KC_LGUI),   S(KC_LCTL), KC_LEFT, KC_DOWN, KC_RGHT),
 
-[1] = LAYOUT_tkl_ansi_tsangan( /* FN */
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_VOLD, KC_MSTP, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, 
-    _______, RM_TOGG, RM_NEXT, RM_HUED, RM_HUEU, RM_SATD, RM_SATU, AG_TOGG, _______, RM_SPDD, RM_SPDU,          _______,                   _______,
-    _______, _______, _______,                   RETRO_RGB,                                            _______, _______, QK_BOOT, _______, _______, _______),
-
-[2] = LAYOUT_tkl_ansi_tsangan( /* scratch, controlled by VIA */
+[_MAC] = LAYOUT_tkl_ansi_tsangan( /* scratch, controlled by VIA */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                   _______,
-    _______, _______, _______,                   _______,                                            _______, _______, _______, _______, _______, _______),
+    _______, KC_LALT, KC_LGUI,                   _______,                                            C(KC_LALT), _______, _______, _______, _______, _______),
+
+[_FN] = LAYOUT_tkl_ansi_tsangan( /* FN */
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_VOLD, KC_MSTP, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, 
+    _______, RM_TOGG, RM_NEXT, RM_HUED, RM_HUEU, RM_SATD, RM_SATU, MAC_TOGGLE, _______, RM_SPDD, RM_SPDU,          _______,                   _______,
+    _______, _______, _______,                   RETRO_RGB,                                            _______, _______, QK_BOOT, _______, _______, _______),
 
 
 [3] = LAYOUT_tkl_ansi_tsangan( /* scratch, controlled by VIA */
@@ -120,7 +123,15 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode >= HED1 && keycode < LIT_END && record->event.pressed) {
+    if (keycode == MAC_TOGGLE && record->event.pressed) {
+        // swap between MAC and BASE layers persistently
+        if (get_highest_layer(default_layer_state) == _MAC) {
+            set_single_persistent_default_layer(_BASE);
+        } else {
+            set_single_persistent_default_layer(_MAC);
+        }
+        return false;
+    } else if (keycode >= HED1 && keycode < LIT_END && record->event.pressed) {
         send_string_P(lit_table[keycode - HED1]);
         return false;
     } else if (keycode == RETRO_RGB) {
