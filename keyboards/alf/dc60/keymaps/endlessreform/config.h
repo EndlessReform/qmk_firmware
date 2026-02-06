aeefe43a35e4a35abc4e28209f540d78e661b18b
Jacob Keisling <jacob@keisling.me>
2026-02-05 19:55:55 -0600

    Remove legacy options

--- keyboards/alf/dc60/keymaps/endlessreform/config.h
+++ keyboards/alf/dc60/keymaps/endlessreform/config.h
@@ -18,3 +18,7 @@
 
 // place overrides here
 #define COMBO_TERM 20
+// enable per-key hold-on-other-key-press handling
+#undef HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+
