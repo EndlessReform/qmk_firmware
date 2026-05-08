9e260dee64b9de288995232205ab1fc3d9ba8811
Jacob Keisling <contact@keisling.me>
2026-05-07 21:38:08 -0500

    WIP fix bottom row for tsangan being improperly shifted

--- keyboards/1upkeyboards/pi60_hse/keymaps/endlessreform/keymap.c
+++ keyboards/1upkeyboards/pi60_hse/keymaps/endlessreform/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,    KC_W,    KC_E,             KC_R,             KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
     LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,             LT(_GIT, KC_F),   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_MD, KC_SCLN), KC_QUOT, KC_ENT,
     KC_LSFT,                  KC_Z,    KC_X,             KC_C,             KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN),
-    LT(_FN, KC_CAPS), KC_LALT, KC_LGUI,                                    KC_SPC,                                RH_COPT, RH_GSFT, RH_CSFT, KC_RCTL
+    LT(_FN, KC_CAPS), KC_LALT, KC_LGUI,                                    KC_SPC,                                _______, RH_COPT, RH_GSFT, RH_CSFT
 ),
 
 [_FN] = LAYOUT_60_ansi_split_bs_rshift(
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,         KC_VOLD, KC_MSTP, KC_VOLU,          _______,          _______, KC_END,  KC_PGDN, KC_PGUP, KC_HOME, _______, _______, _______, KC_DEL,
     _______,         KC_MPRV, KC_MPLY, KC_MNXT,          _______,          _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
     _______,                  _______, _______,          _______,          _______, _______, _______, _______, _______, _______, QK_BOOT, _______, _______,
-    _______,         _______, _______,                                     _______,                               _______, _______, _______, _______
+    _______,         _______, _______,                                     _______,                               _______, _______, _______, QK_BOOT
 ),
 
 [_MD] = LAYOUT_60_ansi_split_bs_rshift(
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TAG,             _______, _______, _______,           REMOTE,           _______, _______, UPSTREAM, _______, ORIGIN,  _______, _______, _______, REVERT,
     PUSH,            ADD,     STATUS,  _______,           _______,          _______, HARD,    _______, _______, _______, _______, _______, COMMIT,
     REBASE,                   _______, _______,           _______,          _______, _______, GIT_MAIN, _______, _______, _______, _______, CHECKOUT, _______,
-    BRANCH,          PULL,    MERGE,                                      _______,                               STASH,   FETCH,   CLONE,  _______
+    BRANCH,          PULL,    MERGE,                                      _______,                               _______, STASH,   FETCH,   CLONE
 ),
 };
 // clang-format on
